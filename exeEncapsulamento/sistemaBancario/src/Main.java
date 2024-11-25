09a007b7855ad09825e5d1941c2c3e3852ca535f
Davi Felicio de Jesus <155579946+Davi-Felicio@users.noreply.github.com>
2024-11-25 11:28:33 -0300

    puxando coisa do meu repositorio

---
+++ exeEncapsulamento/sistemaBancario/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Conta conta1 = new Conta("123", 1000.0);
+        Conta conta2 = new Conta("456", 500.0);
+
+        TransacaoService transacaoService = new TransacaoService();
+        transacaoService.processarTransacao(conta1, conta2, 200.0);
+
+        System.out.println("Saldo conta " + conta1.getNumero() + " : " + conta1.getSaldo());
+        System.out.println("Saldo conta " + conta2.getNumero() + " : " + conta2.getSaldo());
+    }
+}
